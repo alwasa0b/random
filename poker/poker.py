cfaf16969616f01984e701bd1753c037e432244a
Saeed Alalwan <alalwan@gmail.com>
2014-03-26 11:11:06 -0400

    fullHouse needs some work

--- poker/poker.py
+++ poker/poker.py
@@ -1,14 +1,16 @@
 hand1=[['10', 'c'], ['j', 'c'], ['q', 'c'], ['k', 'c'], ['a', 'c']]
 hand2=[[2, 'c'], [3, 'c'], [4, 'c'], [5, 'c'], [7, 'c']]
-hand3=[[10, 'c'], [6, 'c'], [10, 'c'], [6, 'a'], [6, 'a']]
-
-hand4=[[1, 'c'], [1, 'a'], [8, 'o'], [2, 'c'], [2, 'i']]
-hand5=[[1, 'c'], [2, 'c'], [3, 'c'], [4, 'c'], [5, 'c']]
+hand3=[[10, 'c'], [5, 'c'], [5, 'c'], [5, 'a'], [5, 'a']]
+hand4=[[1, 'c'], [1, 'a'], [4, 'o'], [8, 'c'], [8, 'i']]
+hand5=[[1, 'c'], [1, 'c'], [3, 'c'], [4, 'c'], [5, 'c']]
 hand6=[[1, 'c'], [2, 'c'], [3, 'c'], [4, 'c'], [5, 'p']]
-hand7=[[2, 'c'], [8, 'c'], [1, 'c'], [1, 'h'], [2, 'c']]
-hand8=[[2, 'c'], [8, 'c'], [1, 'c'], [5, 'h'], [7, 'c']]
+hand7=[[1, 'c'], [8, 'c'], [1, 'c'], [1, 'h'], [2, 'c']]
+
+hand8=[[1, 'c'], [9, 'c'], [1, 'c'], [5, 'h'], [9, 'c']]
+
 hand9=[[2, 'c'], [8, 'r'], [1, 'i'], [5, 'a'], [7, 'r']]
 hand10=[[2, 'c'], [9, 'r'], [0, 'i'], [5, 'a'], [7, 'r']]
+
 
 ##Done  
 def isRoyalFlush(hand):
@@ -49,16 +51,17 @@ def fulHouse(hand):
     count2 = 0
     count1 = 0
     counter=0
+    
     for i in range(0,4):
         counter=0
         for x in range(i+1,5):
             if hand[i][0]==hand[x][0]:
                     counter+=1
-                    if(counter==2 and count2!=2):
-                        count2 = 2
-                    elif (counter==1 and count1!=1):
-                        count1 = 1  
-                                                  
+        if(counter==2):
+                count2 = 2
+        elif (counter==1):
+                count1 = 1  
+                                             
     if(count1==1 and count2==2):
         return True
     
@@ -77,6 +80,7 @@ def flush(hand):
             return False
         
         return True
+    
 #Done
 def isStraight(hand):
     for i in range(0,4):
@@ -103,13 +107,16 @@ def threeOfAkind(hand):
 #Done        
 def twoPair(hand):  
     count = 0
+    hit=0
     for i in range(0,4):
         count=0
         for x in range(i+1,5):
             if hand[x][0]==hand[i][0]:
                 count+=1
                 if(count==1):
-                    return True  
+                    hit+=1 
+    if hit==2:
+        return True                
     return False
 
 #Done
@@ -137,14 +144,10 @@ def highCard(hand):
 
 print isRoyalFlush(hand1)
 print isStraightFlush(hand2)
-print
-print fulHouse(hand3)
-print fourOfAkind(hand4)
-
+print fourOfAkind(hand3)
+print fulHouse(hand4)
 print flush(hand5)
-
 print isStraight(hand6)
-
 print threeOfAkind(hand7)
 print twoPair(hand8)
 print pair(hand9)
