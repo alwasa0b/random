b2a6bce849f46238e499df3c1365568519ea1fe3
Saeed Alalwan <alalwan@gmail.com>
2014-03-25 21:24:00 -0400

    fixed some issues with isStraight

--- poker/poker.py
+++ poker/poker.py
@@ -2,20 +2,25 @@ hand=[[2, 'c'], [3, 'c'], [4, 'c'], [5, 'c'], [6, 'c']]
 
 
 def isStraight(hand):
-    count1=0
-    count2=0
-    
     for i in range(0,4):
-        if hand[i][1]==hand[i+1][1]:
-            count2+=1
-            print hand[i+1][0]
-        if hand[i][0]==(hand[i+1][0]-1):
-            print hand[i+1][0]
-            count1+=1            
-    if(count1==4 and count2==4):
-        return True
-    else:
-        return False
+        if hand[i][1]==hand[i+1][1] and hand[i][0]==(hand[i+1][0]-1):
+                continue
+        else:
+            return False
+                  
+    return True
+    
 
-            
+
+
+def isRoyalFlush(hand):  
+    for i in range(0,4):
+        if hand[i][1]==hand[i+1][1] and hand[i][0]==(hand[i+1][0]-1):
+                continue
+        else:
+            return False
+                  
+    return True
+    
+          
 print isStraight(hand)
